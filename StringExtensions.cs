d094cc90d0beaa39b157b2cc7e18956899ca213c
Alisher <marhamattojiboyeva5@gmail.com>
2026-06-13 23:28:32 +0500

    Initial commit - NuGet package

---
+++ StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Alisher.StringExtensions;
+
+public static class StringExtensions
+{
+    // Matn ichidagi ortiqcha bo'shliqlarni (probellarni) tozalab beruvchi extension metod
+    public static string CleanSpaces(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return string.Empty;
+
+        // So'zlar orasida bittadan ortiq probel bo'lsa, ularni bitta qilib beradi
+        return string.Join(" ", input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+    }
+}
